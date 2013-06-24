a9b774c714a56f32534b5d60aa3e6b9d093a0856
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-24 16:25:57 -0500

    removed unnecessary models and migrations

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,17 @@
+class UserController < ApplicationController
+  def index
+    @user = User.new
+  end
+
+  def new
+  end
+
+  def create
+    user = User.create_user(params[:user])
+    session[:id] = user.id
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end 
+end
