683c56c217a9e5c636dc56b89888aac5b65be241
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-24 15:11:13 -0500

    created seeds file, post controller, and new index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+users = []
+
+30.times do
+  users << User.create(username: Faker::Internet.user_name,
+              email: Faker::Internet.email,
+              password: BCrypt::Password.create("123"))
+end
+
+posts = []
+
+60.times do
+  posts << Post.create(title: Faker::Lorem.sentence(word_count = 5),
+                       url: Faker::Internet.url)
+end
+
+comments = []
+
+300.times do
+  comments << Comment.create(content: Faker::Lorem.paragraph(sentence_count = 3))
+end
+
+posts.each do |post|
+  post.update_attributes(:user_id => users.sample.id)
+end
+
+comments.each do |comment|
+  comment.update_attributes(:user_id => users.sample.id)
+  comment.update_attributes(:post_id => posts.sample.id)
+end
+
+300.times do
+  Postvote.create(post_id: posts.sample.id, user_id: users.sample.id)
+end
+
+1200.times do
+  Commentvote.create(comment_id: comments.sample.id, user_id: users.sample.id)
+end
