3ae4238344ee3d2607d2a273d8cac04806a4747b
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-24 19:01:20 -0500

    remove bcrypt from seeds, fixed some users_controller action issues

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,14 @@
 class UsersController < ApplicationController
   def index
-    @user = User.new
+    @users = User.all
   end
 
   def new
+    @user = User.new
   end
 
   def create
-    user = User.new(params[:user])
+    @user = User.new(params[:user])
     redirect_to @user if @user.save
   end
   
